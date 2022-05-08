79d204281a2a3e60e67528f222264c86f9ca8e41
Bruno Demarchi <brunodemarchi30@gmail.com>
2022-05-08 17:06:20 -0300

    name update

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,7 +5,7 @@ import { CloseButton } from "../CloseButton";
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
-import { FeedbackTypeStep } from "./Steps/feedbackTypeStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 import { FolderDotted } from "phosphor-react";
 import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
